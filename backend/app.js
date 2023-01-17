fd2ec2734194485695e4ebbccd974439a005889a
ollivarila <olli.varila@gmail.com>
2023-01-17 16:26:45 +0200

    Fixes

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,6 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, 'dist')))
 }
 
-const port = process.env.PORT
+const port = process.env.PORT || 8080
 
 app.listen(port, () => console.log('Server listening on port ' + port))
